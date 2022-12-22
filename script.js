de76b91d6434a43d1d155ae39ce92c7875c8abd2
ParvaCamer <64281030+ParvaCamer@users.noreply.github.com>
2022-12-22 01:20:12 +0100

    new commit
    JavaScript done. Need the design now

--- script.js
+++ script.js
@@ -8,20 +8,33 @@ const combination = [
     [1, 5, 9],
     [7, 5, 3]
 ];
-let playerWhoPlay = ['circle', 'cross'];
-const allDiv = document.querySelectorAll('.game-box');
 
-allDiv.forEach(function (div) {
-    div.addEventListener('click', function () {
-        let valeur = this.dataset.value;
-        console.log(valeur, this.id);
-        addClass('cross', this)
-    })
+let play = true;
+let players = ['circle', 'cross'];
+let playerWhoPlay = players[0];
+let array = {
+    'circle': [],
+    'cross': []
+};
+
+$('.game-box').click(function () {
+    if (play && $(this).hasClass("empty-box")) {
+        $(this).removeClass("empty-box").addClass(playerWhoPlay + "-box").append("<div class=" + playerWhoPlay + "></div>");;
+        let value = $(this).data('value');
+        array[playerWhoPlay].push(value);
+        checkCombination(array[playerWhoPlay]);
+        (playerWhoPlay = [players[1], players[1] = playerWhoPlay][0]);
+    }
 })
 
-function addClass(newClass, div) {
-    const newDiv = document.createElement('div');
-    newDiv.classList.add(newClass);
-    div.appendChild(newDiv);
+function checkCombination(arrayToCheck) {
+    for (let i = 0; i < combination.length; i++) {
+        let isMatch = combination[i].every(element =>
+            arrayToCheck.includes(element));
+        if (isMatch) {
+            console.log('fini ', playerWhoPlay, ' gagne');
+            play = false;
+            break;
+        }
+    }
 }
-
